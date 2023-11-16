4ec3ecbb58835171caf3378298a2fe6bc09f7ed5
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-11-16 15:06:27 +1100

    Adding cmake install.

--- table-rdf-config.cmake
+++ table-rdf-config.cmake
@@ -1,3 +1,3 @@
-# CMake config file to facilitate find_package(table-ws-protocol CONFIG REQURIED) etc.
+# CMake config file to facilitate find_package(table-rdf CONFIG REQURIED) etc.
 # Currently only requires including the auto-generated targets .cmake file.
-include("${CMAKE_CURRENT_LIST_DIR}/table-ws-protocol-targets.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/table-rdf-targets.cmake")
