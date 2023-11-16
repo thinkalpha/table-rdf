ac10ac27988b148d5861368928aee272db215f9f
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-11-17 10:54:47 +1100

    Adding explicit find_package for dependencies to config.cmake.

--- table-rdf-config.cmake
+++ table-rdf-config.cmake
@@ -1,3 +1,9 @@
 # CMake config file to facilitate find_package(table-rdf CONFIG REQURIED) etc.
-# Currently only requires including the auto-generated targets .cmake file.
+
+# Include the auto-generated targets .cmake file.
 include("${CMAKE_CURRENT_LIST_DIR}/table-rdf-targets.cmake")
+
+# Find dependencies.
+find_package(spdlog CONFIG REQUIRED)
+find_package(Boost 1.80.0 REQUIRED)
+find_package(date CONFIG REQUIRED)
