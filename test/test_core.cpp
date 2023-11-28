6fc10173198c58a35ca84b003555e0bb64de8bd9
Jarrod Smith <jarrod.smith@ardentblue.com>
2023-11-28 04:07:41 -0500

    1GB filemapped write / read test passing.

--- test/test_core.cpp
+++ test/test_core.cpp
@@ -226,28 +226,6 @@ TEST_CASE( "record <-> binary file", "[core]" )
   using namespace types;
   namespace ipc = boost::interprocess;
 
-  char const* file_name  = "./test.bin";
-  std::size_t const file_size = 10000;
-
-  ipc::file_mapping::remove(file_name);
-  {  
-    std::ofstream file{file_name, std::ios_base::binary};
-    file.seekp(file_size-1, std::ios_base::beg); // Set the file size.
-    file.put(0);
-  }
-
-  // Create a file mapping.
-  ipc::file_mapping m_file(file_name, ipc::read_write);
-  // Map the whole file with read-write permissions in this process.
-  ipc::mapped_region region(m_file, ipc::read_write);
-  // Get the address of the mapped region.
-  void* addr = region.get_address();
-  std::size_t size = region.get_size();
-
-  // Write all the memory to 1
-  std::memset(addr, 1, size);
-  region.flush();
-
   // Test a descriptor with all supported types.
   rdf::fields_builder builder;
   builder.push({ "Key8 Field",       "Key8 field description",       Key8, 31})
@@ -276,11 +254,27 @@ TEST_CASE( "record <-> binary file", "[core]" )
 
   descriptor d {"All Fields Test Descriptor", builder};
 
-  constexpr auto k_count = 100;
+  // Set up memory mapped file.
+  char const* file_name  = "./test.bin";
+  
+  constexpr auto k_desired_file_size = 1024 * 1024 * 1024; // 1 GB.
+  auto const record_count = k_desired_file_size / d.mem_size();
+  size_t const file_size = record_count * d.mem_size();
 
-  SECTION("record write")
+  ipc::file_mapping::remove(file_name);
+  {  
+    std::ofstream file{file_name, std::ios_base::binary};
+    file.seekp(file_size-1, std::ios_base::beg);         // Set the file size.
+    file.put(0);
+  }
+
+  ipc::file_mapping m_file(file_name, ipc::read_write);  // Create a file mapping.
+  ipc::mapped_region region(m_file, ipc::read_write);    // Map the whole file with read-write permissions in this process.
+  void* mapped_file_addr = region.get_address();         // Get the address of the mapped region.
+  REQUIRE(region.get_size() == file_size);
+
   {
-    mem_t* const mem_alloc = (mem_t*)std::aligned_alloc(field::k_record_alignment, d.mem_size() * k_count);
+    mem_t* const mem_alloc = (mem_t*)boost::alignment::align_up(mapped_file_addr, field::k_record_alignment);
     REQUIRE(boost::alignment::is_aligned(field::k_record_alignment, mem_alloc));
     auto mem = mem_alloc;
 
@@ -288,41 +282,72 @@ TEST_CASE( "record <-> binary file", "[core]" )
 
     auto fields = d.fields();
     
-    // Write to record memory.
-    for (int i = 0; i < k_count; ++i)
+    // Write records to file.
+    for (int64_t i = 0; i < (int64_t)record_count; ++i)
     {
       auto const t = time + timestamp_t::duration{i};
       auto const ui = (uint64_t)i;
 
-      d.fields("Key8 Field")      .write<Key8>      (mem, fmt::format("_B[_{}d:{}d]", i, i + 1));
-      d.fields("Key16 Field")     .write<Key16>     (mem, fmt::format("AAPL_{}:*", i % 10));
-      d.fields("String8 Field")   .write<String8>   (mem, fmt::format("_B[_{}d:{}d]", i + 2, i + 3));
-      d.fields("String16 Field")  .write<String16>  (mem, fmt::format("SPY_{}:*", i % 10));
-      d.fields("Timestamp Field") .write<Timestamp> (mem, t);
-      d.fields("Char Field")      .write<Char>      (mem, 33 + i % (127 - 33));
-      d.fields("Utf_Char8 Field") .write<Utf_Char8> (mem, ui % 256);
-      d.fields("Utf_Char16 Field").write<Utf_Char16>(mem, ui % 512);
-      d.fields("Utf_Char32 Field").write<Utf_Char32>(mem, ui % 1024);
-      d.fields("Int8 Field")      .write<Int8>      (mem, -i << 0);
-      d.fields("Int16 Field")     .write<Int16>     (mem, -i << 1);
-      d.fields("Int32 Field")     .write<Int32>     (mem, -i << 2);
-      d.fields("Int64 Field")     .write<Int64>     (mem, -i << 3);
-      d.fields("Uint8 Field")     .write<Uint8>     (mem, i << 0);
-      d.fields("Uint16 Field")    .write<Uint16>    (mem, i << 1);
-      d.fields("Uint32 Field")    .write<Uint32>    (mem, i << 2);
-      d.fields("Uint64 Field")    .write<Uint64>    (mem, i << 3);
-      d.fields("Float16 Field")   .write<Float16>   (mem, i * 1.f16);
-      d.fields("Float32 Field")   .write<Float32>   (mem, i * 10.f32);
-      d.fields("Float64 Field")   .write<Float64>   (mem, i * 100.f64);
-      d.fields("Float128 Field")  .write<Float128>  (mem, i * 1000.f128);
-      d.fields("Bool Field")      .write<Bool>      (mem, i % 2);
+      d.fields(0ul) .write<Key8>      (mem, fmt::format("_B[_{}d:{}d]", i, i + 1));
+      d.fields(1ul) .write<Key16>     (mem, fmt::format("AAPL_{}:*", i % 10));
+      d.fields(2ul) .write<String8>   (mem, fmt::format("_B[_{}d:{}d]", i + 2, i + 3));
+      d.fields(3ul) .write<String16>  (mem, fmt::format("SPY_{}:*", i % 10));
+      d.fields(4ul) .write<Timestamp> (mem, t);
+      d.fields(5ul) .write<Char>      (mem, 33 + i % (127 - 33));
+      d.fields(6ul) .write<Utf_Char8> (mem, ui % 256);
+      d.fields(7ul) .write<Utf_Char16>(mem, ui % 512);
+      d.fields(8ul) .write<Utf_Char32>(mem, ui % 1024);
+      d.fields(9ul) .write<Int8>      (mem, -i << 0);
+      d.fields(10ul).write<Int16>     (mem, -i << 1);
+      d.fields(11ul).write<Int32>     (mem, -i << 2);
+      d.fields(12ul).write<Int64>     (mem, -i << 3);
+      d.fields(13ul).write<Uint8>     (mem, i << 0);
+      d.fields(14ul).write<Uint16>    (mem, i << 1);
+      d.fields(15ul).write<Uint32>    (mem, i << 2);
+      d.fields(16ul).write<Uint64>    (mem, i << 3);
+      d.fields(17ul).write<Float16>   (mem, i * 1.f16);
+      d.fields(18ul).write<Float32>   (mem, i * 10.f32);
+      d.fields(19ul).write<Float64>   (mem, i * 100.f64);
+      d.fields(20ul).write<Float128>  (mem, i * 1000.f128);
+      d.fields(21ul).write<Bool>      (mem, i % 2);
 
       mem += d.mem_size();
     }
 
-    
+    // Read records from file.
+    mem = mem_alloc;
+    for (int64_t i = 0; i < (int64_t)record_count; ++i)
+    {
+      auto record = rdf::record{mem};
 
-    free(mem_alloc);
+      auto const t = time + timestamp_t::duration{i};
+      auto const ui = (uint64_t)i;
+
+      REQUIRE(record.get<Key8>      (d.fields(0ul))  == fmt::format("_B[_{}d:{}d]", i, i + 1));
+      REQUIRE(record.get<Key16>     (d.fields(1ul))  == fmt::format("AAPL_{}:*", i % 10));
+      REQUIRE(record.get<String8>   (d.fields(2ul))  == fmt::format("_B[_{}d:{}d]", i + 2, i + 3));
+      REQUIRE(record.get<String16>  (d.fields(3ul))  == fmt::format("SPY_{}:*", i % 10));
+      REQUIRE(record.get<Timestamp> (d.fields(4ul))  == t);
+      REQUIRE(record.get<Char>      (d.fields(5ul))  == 33 + i % (127 - 33));
+      REQUIRE(record.get<Utf_Char8> (d.fields(6ul))  == (char8_t)(ui % 256));
+      REQUIRE(record.get<Utf_Char16>(d.fields(7ul))  == (char16_t)(ui % 512));
+      REQUIRE(record.get<Utf_Char32>(d.fields(8ul))  == (char32_t)(ui % 1024));
+      REQUIRE(record.get<Int8>      (d.fields(9ul))  == (int8_t)(-i << 0));
+      REQUIRE(record.get<Int16>     (d.fields(10ul)) == (int16_t)(-i << 1));
+      REQUIRE(record.get<Int32>     (d.fields(11ul)) == (int32_t)(-i << 2));
+      REQUIRE(record.get<Int64>     (d.fields(12ul)) == (int64_t)(-i << 3));
+      REQUIRE(record.get<Uint8>     (d.fields(13ul)) == (uint8_t)(ui << 0));
+      REQUIRE(record.get<Uint16>    (d.fields(14ul)) == (uint16_t)(ui << 1));
+      REQUIRE(record.get<Uint32>    (d.fields(15ul)) == (uint32_t)(ui << 2));
+      REQUIRE(record.get<Uint64>    (d.fields(16ul)) == (uint64_t)(ui << 3));
+      REQUIRE(record.get<Float16>   (d.fields(17ul)) == (std::float16_t)i * 1.f16);
+      REQUIRE(record.get<Float32>   (d.fields(18ul)) == (std::float32_t)i * 10.f32);
+      REQUIRE(record.get<Float64>   (d.fields(19ul)) == (std::float64_t)i * 100.f64);
+      REQUIRE(record.get<Float128>  (d.fields(20ul)) == (std::float128_t)i * 1000.f128);
+      REQUIRE(record.get<Bool>      (d.fields(21ul)) == i % 2);
+
+      mem += d.mem_size();
+    }
   }
 }
 
